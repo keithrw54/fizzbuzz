c6e2fe4a6380647b727379cda92703b0899c71bf
Keith Williams <keithrw@comcast.net>
2018-04-07 07:11:03 -0400

    added test for 0..100

--- spec/lib/fizzbuzz_spec.rb
+++ spec/lib/fizzbuzz_spec.rb
@@ -11,4 +11,7 @@ describe 'fizzbuzz' do
   it 'returns fizzbuzz on 15' do
     expect(fizzbuzz(15)).to include "fizzbuzz"
   end
+  it "runs numbers 1 to 100" do
+    expect{(1..100).each{ |i| puts fizzbuzz(i)}}.to_not raise_error
+  end
 end
