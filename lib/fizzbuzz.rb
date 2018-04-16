3abd363f7fe470d1c408b031775b00ac311de0e1
Keith Williams <keithrw@comcast.net>
2018-04-16 11:56:00 -0400

    add test case for non 3 5 divs. Change code to not return number on fizz or buzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,5 @@
 def fizzbuzz(i)
-  "#{i}: #{i%3==0 ? 'fizz' : ''}#{i%5==0 ? 'buzz' : ''}"
+  s = "#{i%3==0 ? 'fizz' : ''}#{i%5==0 ? 'buzz' : ''}"
+  s = i if i%3 != 0 && i%5 != 0
+  s
 end
--- spec/lib/fizzbuzz_spec.rb
+++ spec/lib/fizzbuzz_spec.rb
@@ -10,6 +10,11 @@ describe 'fizzbuzz' do
   it 'returns fizzbuzz on 15' do
     expect(fizzbuzz(15)).to include "fizzbuzz"
   end
+  it 'returns the number on most' do
+    [1,2,4,7,8,11,13,14].each do |i|
+      expect(fizzbuzz(i)).to eq(i)
+    end
+  end
   it "runs numbers 1 to 100" do
     expect{(1..100).each{ |i| puts fizzbuzz(i)}}.to_not raise_error
   end
