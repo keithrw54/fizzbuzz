5e249fc894c3d0b7291cc4d6e5b4ea81bb473975
Keith Williams <kwilliams@cloudhealthtech.com>
2020-05-05 14:18:43 +0000

    fix test name to indicate #. Minor refactor setting the initial string

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,4 @@
-def fizzbuzz(i)
-  s = ''
+def fizzbuzz(i, s='')
   s << 'fizz' if i%3==0
   s << 'buzz' if i%5==0
   s = i if s.empty?
--- spec/lib/fizzbuzz_spec.rb
+++ spec/lib/fizzbuzz_spec.rb
@@ -1,17 +1,17 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns fizz on 3' do
+  it '3 gives' do
     expect(fizzbuzz(3)).to include "fizz"
   end
-  it 'returns buzz on 5' do
+  it '5 gives buzz' do
     expect(fizzbuzz(5)).to include "buzz"
   end
-  it 'returns fizzbuzz on 15' do
+  it '15 gives fizzbuzz' do
     expect(fizzbuzz(15)).to include "fizzbuzz"
   end
-  it 'returns the number on most' do
-    [1,2,4,7,8,11,13,14].each do |i|
+  [1,2,4,7,8,11,13,14].each do |i|
+    it "just the number on #{i}" do
       expect(fizzbuzz(i)).to eq(i)
     end
   end
